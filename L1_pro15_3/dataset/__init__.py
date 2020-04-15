7460e2c928fb4686132d7ee013378b920980ee24
z-hXu <791411501@qq.com>
2020-04-15 14:05:26 +0800

    0

---
+++ L1_pro15_3/dataset/__init__.py
@@ -0,0 +1,2 @@
+from .dataset import load_data
+#from .cifar10 import get_cifar_iter_dali as get_cifar
---
+++ L1_pro16_3/run.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=2 nohup python -u main.py \
+--model resnet20_1w1a \
+--results_dir ./ \
+--save result \
+--epoch 405 \
+--lr 0.1 \
+-b 256 \
+> resnet20_L16_3.log 2>&1 &
